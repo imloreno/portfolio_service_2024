699081349d93a7e3d5c3198fd90452bde8e39446
Lorenzo Arias Villegas <santyago.arias.sav@gmail.com>
2024-08-14 11:10:07 -0400

    Skills and SkillSet tables

--- src/main/java/com/projects/portfolio/portfolio/models/SkillSet.java
+++ src/main/java/com/projects/portfolio/portfolio/models/SkillSet.java
@@ -1,24 +1,23 @@
 package com.projects.portfolio.portfolio.models;
 
-import com.projects.portfolio.portfolio.constants.ProjectConst.SkillCategory;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.UUID;
 
 @Entity
 @Data
-@Table(name = "skills")
-public class SkillSet {
+@Table(name = "skill_set")
+public class SkillSet implements Serializable {
    @Id
    @GeneratedValue(strategy = GenerationType.AUTO)
-   @Column(name = "id")
    private UUID id;
-   @Column(columnDefinition = "DATE", nullable = false)
+   @Column(name = "started_from", columnDefinition = "DATE", nullable = false)
    private Date from;
-   @Column(columnDefinition = "DATE")
+   @Column(name = "terminated_at", columnDefinition = "DATE")
    private Date to;
-   @Column(length = 15, nullable = false)
-   private SkillCategory category;
+   @Column(length = 20, nullable = false)
+   private String category;
 }
--- src/main/java/com/projects/portfolio/portfolio/models/Skills.java
+++ src/main/java/com/projects/portfolio/portfolio/models/Skills.java
@@ -1,20 +1,20 @@
 package com.projects.portfolio.portfolio.models;
 
-import com.projects.portfolio.portfolio.constants.ProjectConst;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Entity
 @Data
 @Table(name = "skills")
-public class Skills {
+public class Skills implements Serializable {
    @Id
    @GeneratedValue(strategy = GenerationType.AUTO)
    @Column(name = "id")
    private UUID id;
 
-   @Column(unique = true, length = 10, nullable = false)
-   private ProjectConst.Skills name;
+   @Column(unique = true, length = 20, nullable = false)
+   private String skill;
 }
